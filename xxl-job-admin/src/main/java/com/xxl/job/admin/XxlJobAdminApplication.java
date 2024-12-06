fea626d14cc7f973d9fd9400bba504ff32ab6b66
itbeien <baintong@aliyun.com>
2024-12-06 08:54:56 +0800

    提交整合nacos配置中心的xxl-job-admin源码，方便和定时任务执行器整合

---
+++ xxl-job-admin/src/main/java/com/xxl/job/admin/XxlJobAdminApplication.java
@@ -0,0 +1,19 @@
+package com.xxl.job.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 项目网站：https://www.itbeien.cn
+ * 公众号：贝恩聊架构
+ * 全网同名，欢迎小伙伴们关注
+ */
+@SpringBootApplication
+public class
+XxlJobAdminApplication {
+
+	public static void main(String[] args) {
+        SpringApplication.run(XxlJobAdminApplication.class, args);
+	}
+
+}
