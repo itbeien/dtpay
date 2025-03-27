7e075cd147d39d4456c501397ebd506e6ed0050b
itbeien <baintong@aliyun.com>
2025-03-27 20:10:17 +0800

    创建启动类

---
+++ dtpay-merchant/src/main/java/cn/itbeien/merchant/DmBoot.java
@@ -0,0 +1,21 @@
+package cn.itbeien.merchant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author itbeien
+ * 项目网站：https://www.itbeien.cn
+ * 公众号：贝恩聊架构
+ * 全网同名，欢迎小伙伴们关注
+ * Java/AI学习社群
+ * dtpay2.0 基于JDK17/21 SpringBoot3
+ * 商户系统-启动入口
+ * Copyright© 2025 itbeien
+ */
+@SpringBootApplication
+public class DmBoot {
+    public static void main(String[] args) {
+        SpringApplication.run(DmBoot.class,args);
+    }
+}
