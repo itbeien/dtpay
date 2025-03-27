0db9322d0f601a293dd24aef73f943600a6d9a36
itbeien <baintong@aliyun.com>
2025-03-27 19:23:23 +0800

    移除历史代码

--- dtpay-api-service/src/main/java/cn/itbeien/api/service/merchant/IMerchantService.java
+++
@@ -1,7 +0,0 @@
-package cn.itbeien.api.service.merchant;
-
-import cn.itbeien.api.entity.merchant.MerchantInfo;
-
-public interface IMerchantService {
-    int saveMerchant(MerchantInfo merchantInfo);
-}
--- dtpay-manager/src/main/java/cn/itbeien/common/core/domain/model/RegisterBody.java
+++
@@ -1,9 +0,0 @@
-package cn.itbeien.common.core.domain.model;
-
-/**
- * 用户注册对象
- */
-public class RegisterBody extends LoginBody
-{
-
-}
