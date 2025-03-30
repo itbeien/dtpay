2471c6bf654e24983ab6a89dfba187f89baa40c9
itbeien <baintong@aliyun.com>
2025-03-30 23:12:06 +0800

    报表，定时任务系统

---
+++ dtpay-task/src/main/java/cn/itbeien/TaskBoot.java
@@ -0,0 +1,21 @@
+package cn.itbeien;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author itbeien
+ * 项目网站：https://www.itbeien.cn
+ * 公众号：贝恩聊架构
+ * 全网同名，欢迎小伙伴们关注
+ * Java/AI/支付系统/SAAS多租户基础技术平台学习社群
+ * Copyright© 2025 itbeien
+ */
+@SpringBootApplication
+@MapperScan
+public class TaskBoot {
+    public static void main(String[] args) {
+        SpringApplication.run(TaskBoot.class, args);
+    }
+}
