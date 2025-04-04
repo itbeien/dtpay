a3fd7c28e0276bc388759b919558d71220b2a22c
itbeien <baintong@aliyun.com>
2025-04-04 21:16:37 +0800

    更新注释

--- dtpay-task/src/main/java/cn/itbeien/task/mapper/trade/tradeorder/TradeOrderMapper.java
+++ dtpay-task/src/main/java/cn/itbeien/task/mapper/trade/tradeorder/TradeOrderMapper.java
@@ -4,7 +4,14 @@ import cn.itbeien.common.entity.trade.TradeOrder;
 import cn.itbeien.task.vo.trade.TradeOrderQryPar;
 
 import java.util.List;
-
+/**
+ * @author itbeien
+ * 项目网站：https://www.itbeien.cn
+ * 公众号：贝恩聊架构
+ * 全网同名，欢迎小伙伴们关注
+ * Java/AI/支付系统/SAAS多租户基础技术平台学习社群
+ * Copyright© 2025 itbeien
+ */
 public interface TradeOrderMapper {
     int deleteByPrimaryKey(String orderId);
 
