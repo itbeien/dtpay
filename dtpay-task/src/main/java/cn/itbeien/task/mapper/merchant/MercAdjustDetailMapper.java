04c97dd0e9f938c4b3024773624e7196ab2372e9
itbeien <baintong@aliyun.com>
2025-04-16 22:00:52 +0800

    新增持久层和vo

---
+++ dtpay-task/src/main/java/cn/itbeien/task/mapper/merchant/MercAdjustDetailMapper.java
@@ -0,0 +1,29 @@
+package cn.itbeien.task.mapper.merchant;
+
+import cn.itbeien.common.entity.merchant.MercAdjustDetail;
+import cn.itbeien.task.vo.merchant.MercAdjustDetailQryPar;
+
+import java.util.List;
+/**
+ * @author itbeien
+ * 项目网站：https://www.itbeien.cn
+ * 公众号：贝恩聊架构
+ * 全网同名，欢迎小伙伴们关注
+ * Java/AI/支付系统/SAAS多租户基础技术平台学习社群
+ * Copyright© 2025 itbeien
+ */
+public interface MercAdjustDetailMapper {
+    int deleteByPrimaryKey(String id);
+
+    int insert(MercAdjustDetail record);
+
+    int insertSelective(MercAdjustDetail record);
+
+    MercAdjustDetail selectByPrimaryKey(String id);
+
+    int updateByPrimaryKeySelective(MercAdjustDetail record);
+
+    int updateByPrimaryKey(MercAdjustDetail record);
+    
+    List<MercAdjustDetail> selectMercAdjustDetailList(MercAdjustDetailQryPar infoQryPar);
+}
