a1f280292a5f5cce66e969d5d0768bf8f2c35496
itbeien <baintong@aliyun.com>
2025-04-23 21:17:49 +0800

    新增实体类

---
+++ dtpay-common/src/main/java/cn/itbeien/common/entity/common/OrgInfo.java
@@ -0,0 +1,88 @@
+package cn.itbeien.common.entity.common;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class OrgInfo implements Serializable {
+    private String orgNo;
+
+    private String orgName;
+
+    private String orgType;
+
+    private String status;
+
+    private Date createTime;
+
+    private String creator;
+
+    private Date updateTime;
+
+    private String updater;
+
+    private static final long serialVersionUID = 1L;
+
+    public String getOrgNo() {
+        return orgNo;
+    }
+
+    public void setOrgNo(String orgNo) {
+        this.orgNo = orgNo == null ? null : orgNo.trim();
+    }
+
+    public String getOrgName() {
+        return orgName;
+    }
+
+    public void setOrgName(String orgName) {
+        this.orgName = orgName == null ? null : orgName.trim();
+    }
+
+    public String getOrgType() {
+        return orgType;
+    }
+
+    public void setOrgType(String orgType) {
+        this.orgType = orgType == null ? null : orgType.trim();
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status == null ? null : status.trim();
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getCreator() {
+        return creator;
+    }
+
+    public void setCreator(String creator) {
+        this.creator = creator == null ? null : creator.trim();
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public String getUpdater() {
+        return updater;
+    }
+
+    public void setUpdater(String updater) {
+        this.updater = updater == null ? null : updater.trim();
+    }
+}
